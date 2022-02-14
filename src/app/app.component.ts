93ecb8f617fb2af1535d2569e020fb84c74b7881
sanjibanroy53@gmail.com <sanjibanroy53@gmail.com>
2022-02-14 18:06:30 +0530

    master

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { BackgroundGeolocation, BackgroundGeolocationConfig, BackgroundGeolocationEvents, BackgroundGeolocationResponse } from '@ionic-native/background-geolocation/ngx';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { HttpClient } from '@angular/common/http';
+import { finalize,map  } from 'rxjs/operators';
+import { Component,OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
+
 export class AppComponent {
-  constructor() {}
+  config: BackgroundGeolocationConfig = {
+    desiredAccuracy: 10,
+    stationaryRadius: 2, //I reduce this from 20 to 2
+    distanceFilter: 3, //I reduce this from 30 to 3
+    interval: 2000, //This is missing from example
+    debug: true, //  enable this hear sounds for background-geolocation life-cycle.
+    stopOnTerminate: false, // enable this to clear background location settings when the app terminates
+  };
+  constructor(  private backgroundGeolocation: BackgroundGeolocation,
+    private geolocation: Geolocation,
+    private http:HttpClient) {}
+
+  OnInit(){
+    
+  }
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 import { BackgroundGeolocation, BackgroundGeolocationConfig, BackgroundGeolocationEvents, BackgroundGeolocationResponse } from '@ionic-native/background-geolocation/ngx';
 import { Geolocation } from '@ionic-native/geolocation/ngx';
 import { HttpClient } from '@angular/common/http';
@@ -10,26 +10,52 @@ import { finalize,map  } from 'rxjs/operators';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  // config: BackgroundGeolocationConfig = {
+  //   //desiredAccuracy: 20,
+  //   stationaryRadius: 20,
+  //   distanceFilter: 30,
+  //   debug: false, //  enable this hear sounds for background-geolocation life-cycle.
+  //   stopOnTerminate: false, 
+  //   startForeground: true,
+  //   notificationTitle: 'Location set by GPS',   // <-- android only, customize the title of the notification
+  //   notificationText: 'ENABLED',   
+  //   notificationsEnabled: false, // enable this to clear background location settings when the app terminates
+  //   interval: 10000,
+  //   fastestInterval: 5000,
+  //   activitiesInterval: 10000,
+    
+  // };
+  requestOptions:any
   config: BackgroundGeolocationConfig = {
-    //desiredAccuracy: 20,
-    stationaryRadius: 20,
-    distanceFilter: 30,
-    debug: false, //  enable this hear sounds for background-geolocation life-cycle.
-    stopOnTerminate: false, 
-    startForeground: true,
-    notificationTitle: 'Location set by GPS',   // <-- android only, customize the title of the notification
-    notificationText: 'ENABLED',   
-    notificationsEnabled: false, // enable this to clear background location settings when the app terminates
-    interval: 10000,
-    fastestInterval: 5000,
-    activitiesInterval: 10000,
+    desiredAccuracy: 100,
+    stationaryRadius: 1, //I reduce this from 20 to 2
+    distanceFilter: 3, //I reduce this from 30 to 3
+    interval: 1000, //This is missing from example
+    debug: true, //  enable this hear sounds for background-geolocation life-cycle.
+    stopOnTerminate: false, // enable this to clear background location settings when the app terminates
+    fastestInterval: 1000,
+    activitiesInterval: 2000,
+    url: 'https://nodeserver.nesdr.gov.in/locations',
+    syncUrl: 'https://nodeserver.nesdr.gov.in/sync',
+    syncThreshold: 100,
+    httpHeaders: {
+      'X-FOO': 'bar'
+    },
+    // customize post properties
+    postTemplate: {
+      lat: '@latitude',
+      lon: '@longitude',
+      foo: 'Sanjiban' // you can also add your own properties
+    }
+    
   };
+
   constructor(
     private backgroundGeolocation: BackgroundGeolocation,
     private geolocation: Geolocation,
     private http:HttpClient
     ) {
-
+      
       this.backgroundGeolocation.configure(this.config).then(() => {
         this.backgroundGeolocation.on(BackgroundGeolocationEvents.location).subscribe((location: BackgroundGeolocationResponse) => {
         console.log('Locations', location);
@@ -41,36 +67,86 @@ export class HomePage {
           // this.backgroundGeolocation.finish(); // FOR IOS ONLY
           var lat =location.latitude;
           var lon =location.longitude
+
+          // var myHeaders = new Headers();
+          // myHeaders.append("X-FOO", "bar");
+          // myHeaders.append("Content-Type", "application/json");
+
+          // var raw = JSON.stringify({
+          //   "latitude": 25.6768,
+          //   "longitude": 91.927,
+          //   "foo": "bar"
+          // });
+
+          // this.requestOptions = {
+          //   method: 'POST',
+          //   headers: myHeaders,
+          //   body: raw,
+          //   redirect: 'follow'
+          // };
+
+          // fetch("http://192.168.0.100:3000/locations", this.requestOptions)
+          //   .then(response => response.text())
+          //   .then(result => console.log(result))
+          //   .catch(error => console.log('error', error));
+          
+          //  const formData = new FormData();
+          //   formData.append('lat', String(lat));
+          //   formData.append('lon',  String(lon));
+          //   formData.append('name',  'Sanjiban');
+
+          //   fetch('https://mobileapp.nesdr.gov.in/ASDMA/locationtest/locationtest1.php?lat='+String(lat)+'&lon='+String(lon)+'&name=sanjiban').then(res => res.json())
+          //       .then(json => {
+                  
+          //         console.log(json)  
+            
+          //   });
+                        
+            // this.http.post("https://mobileapp.nesdr.gov.in/ASDMA/locationtest.php", formData)
+            //     .pipe(
+            //         finalize(() => {
+                       
+            //         })
+            //     )
+            //     .subscribe(res => { 
+            //       console.log(res);
+            //   });
+
+
           // this.BackgroundGeolocation.startTask(function(taskKey) {
           //   // execute long running task
           //   // eg. ajax post location
           //   // IMPORTANT: task has to be ended by endTask
           //   BackgroundGeolocation.endTask(taskKey);
           // });
-          
-          this.backgroundGeolocation.startTask().then((taskKey) => {
-            // execute long running task
-            // eg. ajax post location
-            // IMPORTANT: task has to be ended by endTask
-            //this.backgroundGeolocation.endTask(taskKey);
-            setInterval(function() {
-            var lat =location.latitude;
-            var lon =location.longitude
-            const formData = new FormData();
-            formData.append('lat', String(lat));
-            formData.append('lon',  String(lon));
-            formData.append('name',  'Sanjiban');
+
+          //********************************************************************************** */
+          // this.backgroundGeolocation.startTask().then((taskKey) => {
+          //   var lat =location.latitude;
+          //   var lon =location.longitude
+            // const formData = new FormData();
+            // formData.append('lat', String(lat));
+            // formData.append('lon',  String(lon));
+            // formData.append('name',  'Sanjiban');
             
-            this.http.post("https://mobileapp.nesdr.gov.in/ASDMA/locationtest.php", formData)
-                .pipe(
-                    finalize(() => {
+            // this.http.post("https://mobileapp.nesdr.gov.in/ASDMA/locationtest.php", formData)
+            //     .pipe(
+            //         finalize(() => {
                        
-                    })
-                )
-                .subscribe(res => { 
+            //         })
+            //     )
+            //     .subscribe(res => { 
                   
-              });
-            }, 10000);
+            //   });
+          //      fetch('https://mobileapp.nesdr.gov.in/ASDMA/locationtest/locationtest1.php?lat='+String(lat)+'&lon='+String(lon)+'&name=sanjiban').then(res => res.json())
+          //       .then(json => {
+                  
+          //         console.log(json)  
+            
+          //   });
+          // });
+          //********************************************************************************** */
+
             //setInterval(() => this.updatelocation(location.latitude,location.longitude,taskKey), 10000);
             // var lat =location.latitude;
             // var lon =location.longitude
@@ -91,7 +167,7 @@ export class HomePage {
 
             
 
-          });
+         // });
           
 
           //this.updatelocation(location.latitude,location.longitude)
@@ -102,6 +178,9 @@ export class HomePage {
       
 
     }
+    OnInit(){
+      this.backgroundGeolocation.start();
+    } 
     start(){
       alert("Start");
       this.backgroundGeolocation.start();
